b7cd32ee4e95cc8e1b6906d9f117dcbd59dc63a4
Vikastgn <vikamarygina@yandex.ru>
2025-02-11 16:14:16 +0300

    add pagination

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_URL
-console.log("API Key:", API_KEY);
-export const getNews = async () => {
+
+
+export const getNews = async (page_number = 1, page_size = 10) => {
     try {
-        const response = await axios.get(`${BASE_URL}latest-news`, {
+        const response = await axios.get(`${BASE_URL}search`, {
             headers: {
-                'Authorization': `${API_KEY}` // Используйте заголовок Authorization, если это требуется
+                'Authorization': `${API_KEY}`,
+            },
+            params: {
+                page_number,
+                page_size,
             }
 
         })
